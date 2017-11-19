29894022a1ba6337dc80e3aa53e5c4854c5bb6c2
ranyiran <271536025@qq.com>
2017-11-19 14:34:26 +0800

    在drawable里面添加了img_login_text.png和img_regist_text.png文件。 添加了LoginActivity和布局。 //同时修改AndroidManifest.xml里面的LoginActivity的:AppTheme=NoActionBar。 更改了RegistActivity布局。

---
+++ app/src/main/java/com/ran/thewal/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.ran.thewal.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ran.thewal.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
